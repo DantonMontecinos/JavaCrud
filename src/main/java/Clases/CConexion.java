5b706fe9bf290966d694a4adb223c964b5288357
DantonMontecinos <dmontecinos28@gmail.com>
2024-05-07 13:58:26 -0300

    Inicio del crud con JAVA

---
+++ src/main/java/Clases/CConexion.java
@@ -0,0 +1,36 @@
+
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+public class CConexion {
+    
+    Connection conectar =null;
+    
+      String usuario = "root";
+   String contrasenia = "1234";
+   String bd = "bdusuarios";
+   String ip = "localhost";
+   String puerto = "3306";
+   
+   String cadena ="jdbc:mysql://"+ip+":"+puerto+"/"+bd;
+   
+   public Connection establecerConection(){
+       
+       try{
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           conectar = DriverManager.getConnection(cadena,usuario,contrasenia);
+           /*
+           JOptionPane.showMessageDialog(null,"Conexion exitosa");
+           */
+           System.out.println("Conexion exitosa");
+       }catch(Exception e){
+           JOptionPane.showMessageDialog(null,"Error al conectar, "+e.toString());
+       }
+       return conectar;
+   }
+}
+
---
+++ src/main/java/com/mycompany/crud/Crud.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.crud;
+
+import Formularios.FormPrincipal;
+
+
+public class Crud {
+
+    public static void main(String[] args) {
+       FormPrincipal f = new FormPrincipal();
+       f.setVisible(true);
+    }
+}
